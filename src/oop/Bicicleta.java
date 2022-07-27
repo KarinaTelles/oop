6bee6b1b15878bbe49a770ccd1c639eccac159de
Karina Telles <karinatelles.codeit@sulamerica.com.br>
2022-07-27 09:38:19 -0300

    commit inicial

---
+++ src/oop/Bicicleta.java
@@ -0,0 +1,32 @@
+package oop;
+
+public class Bicicleta {
+    int cadencia = 0;
+    int marcha = 0;
+    int velocidade = 0;
+    public Bicicleta() {
+        this.cadencia = 0;
+        this.marcha = 1;
+        this.velocidade = 0;
+    }
+//    public Bicicleta (int cadencia, int marcha, int velocidade)
+//    {
+//        this.cadencia = cadencia;
+//        this.marcha = marcha;
+//        this.velocidade = velocidade;
+//    }
+    public void mudaACadencia(int novaCadencia){
+        this.cadencia = novaCadencia;
+    }
+    public void mudaMarcha(int novaMarcha){
+        if (novaMarcha > 6){
+            this.marcha = 6;
+        }
+        else if (novaMarcha < 0){
+            this.marcha = 1;
+        }
+        else{
+            this.marcha = novaMarcha;
+        }
+    }
+}
---
+++ src/oop/Main.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+//        Bicicleta bikeAzulzinha = new Bicicleta(1,1,1);
+        Bicicleta bikeVazia = new Bicicleta();
+        bikeVazia.mudaACadencia(7);
+    }
+}
